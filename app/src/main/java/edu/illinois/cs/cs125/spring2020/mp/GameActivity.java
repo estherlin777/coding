4c84282a8328c3e8114022152a35d6510c6681f5
estherlin777 <estherl3@illinois.edu>
2020-03-01 15:50:26 -0600

    i'd better get an 80

--- app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/GameActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/GameActivity.java
@@ -28,7 +28,6 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
