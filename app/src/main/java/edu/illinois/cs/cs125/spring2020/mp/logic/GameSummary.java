d6dda36fa3cc7c984796847b5128bd42f7438912
estherlin777 <estherl3@illinois.edu>
2020-03-08 14:46:14 -0500

    yay

--- app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/logic/GameSummary.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/logic/GameSummary.java
@@ -3,7 +3,10 @@ package edu.illinois.cs.cs125.spring2020.mp.logic;
 import android.content.Context;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
+import edu.illinois.cs.cs125.spring2020.mp.R;
 
 public class GameSummary {
     /**JsonObject info.*/
@@ -15,6 +18,7 @@ public class GameSummary {
      * */
     public GameSummary(final com.google.gson.JsonObject infoFromServer) {
         info = infoFromServer;
+        players = info.get("players").getAsJsonArray();
     }
     /**Gets the unique, server-assigned ID of this game.
      * @return the game ID.*/
@@ -45,18 +49,45 @@ public class GameSummary {
      * @param context - an Android context (for access to resources)
      * @return the human-readable team/role name of the user in this game.*/
     public String getPlayerRole(final String userEmail, final Context context) {
+        for (JsonElement p : players) {
+            JsonObject player = (JsonObject) p;
+            if (player.get("email").getAsString().equals(userEmail)) {
+                String[] teamNames = context.getResources().getStringArray(R.array.team_choices);
+                for (int i = 0; i < teamNames.length; i++) {
+                    return teamNames[player.get("team").getAsInt()];
+                }
+            }
+        }
         return null;
     }
     /**Determines whether this game is an invitation to the user.
      * @param userEmail - the logged-in user's email
      * @return whether the user is invited to this game.*/
     public boolean isOngoing(final String userEmail) {
-        return true;
+        for (JsonElement p : players) {
+            JsonObject player = (JsonObject) p;
+            if (player.get("email").getAsString() == userEmail) {
+                if (player.get("state").getAsInt() != GameStateID.ENDED) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     /**Determines whether the user is currently involved in this game
      * @param userEmail - the logged-in user's email
      * @return whether this game is ongoing for the user*/
-    public boolean isInvitation(String userEmail) {
-        return true;
+    public boolean isInvitation(final String userEmail) {
+        if (info.get("state").getAsInt() != GameStateID.ENDED) {
+            for (JsonElement p : players) {
+                JsonObject player = (JsonObject) p;
+                if (player.get("email").getAsString() == userEmail) {
+                    if (player.get("state").getAsInt() == PlayerStateID.INVITED) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
