50df411166938d42acd814b1761de7a5be8dafa2
estherlin777 <estherl3@illinois.edu>
2020-03-08 13:56:37 -0500

    wow

--- app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/logic/GameSummary.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/logic/GameSummary.java
@@ -2,35 +2,46 @@ package edu.illinois.cs.cs125.spring2020.mp.logic;
 
 import android.content.Context;
 
+import com.google.gson.JsonObject;
+
 public class GameSummary {
-    /**variable in com.google.gson.JsonObject.*/
-    private com.google.gson.JsonObject hi;
+    /**JsonObject info.*/
+    private JsonObject info;
     /**Creates a game summary from JSON from the server.
      * @param infoFromServer - one object from the array in the /games response.
      * */
     public GameSummary(final com.google.gson.JsonObject infoFromServer) {
-        hi = infoFromServer;
+        info = infoFromServer;
     }
     /**Gets the unique, server-assigned ID of this game.
      * @return the game ID.*/
     public String getId() {
+        if (info.get("id").getAsString() != null) {
+            return info.get("id").getAsString();
+        }
         return null;
     }
     /**Gets the mode of this game, either area or target.
      * @return the game mode*/
     public String getMode() {
+        if (info.get("mode").getAsString() != null) {
+            return info.get("mode").getAsString();
+        }
         return null;
     }
     /**Gets the owner/creator of this game.
      * @return the email of the game's owner*/
     public String getOwner() {
+        if (info.get("owner").getAsString() != null) {
+            return info.get("owner").getAsString();
+        }
         return null;
     }
     /**Gets the name of the user's team/role.
      * @param userEmail - the logged-in user's email
      * @param context - an Android context (for access to resources)
      * @return the human-readable team/role name of the user in this game.*/
-    public String getPlayerRole(final String userEmail, final Context x) {
+    public String getPlayerRole(final String userEmail, final Context context) {
         return null;
     }
     /**Determines whether this game is an invitation to the user.
