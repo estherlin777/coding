92a244841092c8cf6f8b86aca6b9c177a7fee8a0
estherlin777 <estherl3@illinois.edu>
2020-03-08 15:16:41 -0500

    plz checkstyle

--- app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/LaunchActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/LaunchActivity.java
@@ -3,7 +3,7 @@ package edu.illinois.cs.cs125.spring2020.mp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**public class.*/
 public class LaunchActivity extends AppCompatActivity {
     /**overide.*/
     @Override
