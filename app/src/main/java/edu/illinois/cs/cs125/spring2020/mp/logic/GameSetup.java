e528b548a1acde443d797e8b7fbbc52bb37b96e0
estherlin777 <estherl3@illinois.edu>
2020-03-28 18:35:43 -0500

    JsonAreaandTarget

--- app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/logic/GameSetup.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2020/mp/logic/GameSetup.java
@@ -2,12 +2,31 @@ package edu.illinois.cs.cs125.spring2020.mp.logic;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import java.util.List;
 
 /**public class.*/
 public class GameSetup {
+    /**mode.*/
+    private String mode;
+    /**object array of invitees.*/
+    private Object[] invitees;
+    /**proximity threshold.*/
+    private int proximityThreshold;
+    /**object array of targets.*/
+    private Object[] targets;
+    /**cell size.*/
+    private int cellSize;
+    /**north.*/
+    private double areaNorth;
+    /**east.*/
+    private double areaEast;
+    /**south.*/
+    private double areaSouth;
+    /**west.*/
+    private double areaWest;
     /**constructor for GameSetup.*/
     public GameSetup() {
 
@@ -21,8 +40,25 @@ public class GameSetup {
      * @return a JSON object usable by the /games/create endpoint or null if the configuration is invalid
      * */
     public static JsonObject areaMode(final List<Invitee> invitees, final LatLngBounds area, final int cellSize) {
-        JsonObject json = new JsonObject();
-        return json;
+        JsonArray players = new JsonArray();
+        if (invitees.size() < 1 || !(cellSize > 0)) {
+            return null;
+        }
+        JsonObject areaMode = new JsonObject();
+        areaMode.addProperty("mode", "area");
+        areaMode.addProperty("cellSize", cellSize);
+        areaMode.addProperty("areaNorth", area.northeast.latitude);
+        areaMode.addProperty("areaEast", area.northeast.longitude);
+        areaMode.addProperty("areaSouth", area.southwest.latitude);
+        areaMode.addProperty("areaWest", area.southwest.longitude);
+        for (Invitee person : invitees) {
+            JsonObject invite = new JsonObject();
+            invite.addProperty("email", person.getEmail());
+            invite.addProperty("team", person.getTeamId());
+            players.add(invite);
+        }
+        areaMode.add("invitees", players);
+        return areaMode;
     }
     /** Creates a JSON object representing the configuration of a multiplayer target mode game.
      * Refer to our API documentation for the structure of the output JSON.
@@ -36,7 +72,28 @@ public class GameSetup {
      */
     public static JsonObject targetMode(final List<Invitee> invitees, final List<LatLng> targets,
                                         final int proximityThreshold) {
-        JsonObject json = new JsonObject();
-        return json;
+        if (invitees.size() < 1 || targets.size() < 1 || proximityThreshold <= 1) {
+            return null;
+        }
+        JsonObject target = new JsonObject();
+        target.addProperty("mode", "target");
+        target.addProperty("proximityThreshold", proximityThreshold);
+        JsonArray targetArray = new JsonArray();
+        for (LatLng a: targets) {
+            JsonObject save = new JsonObject();
+            save.addProperty("latitude", a.latitude);
+            save.addProperty("longitude", a.longitude);
+            targetArray.add(save);
+        }
+        target.add("targets", targetArray);
+        JsonArray players = new JsonArray();
+        for (Invitee person : invitees) {
+            JsonObject invite = new JsonObject();
+            invite.addProperty("email", person.getEmail());
+            invite.addProperty("team", person.getTeamId());
+            players.add(invite);
+        }
+        target.add("invitees", players);
+        return target;
     }
 }
